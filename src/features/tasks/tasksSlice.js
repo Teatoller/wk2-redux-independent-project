a7bfe2cdf764c67b1c7ea9ee03354329be8a8f90
stevenennis <seotieno@safaricom.co.ke>
2025-01-28 13:35:21 +0300

    ft(add task): update reducer with add task action
    - add task action in reducer

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -2,15 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   tasks: [
-    { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Build a project", completed: false },
+    { id: 1, title: "Learn React", completed: false },
+    { id: 2, title: "Build a project", completed: false },
   ],
 };
 
 const tasksSlice = createSlice({
   name: "tasks",
   initialState,
-    reducers: {},
+    reducers: {
+      addTask: (state, action) => {
+        state.tasks.push(action.payload);
+      },
+    },
 });
-
+export const { addTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
