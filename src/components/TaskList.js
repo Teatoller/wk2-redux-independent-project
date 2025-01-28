7e677b6a24f814207e8c4cd920cd8bc19c17ac0c
stevenennis <seotieno@safaricom.co.ke>
2025-01-28 12:37:08 +0300

    ft(tasklist): get tasks from store
    - fetch tasks from store and display in tasklist

---
+++ src/components/TaskList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const TaskList = () => {
+  const tasks = useSelector((state) => state.tasks.tasks);
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          {task.text}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
